8c03c23a33317b86ab2c3adfa2cc1f5a3923645a
randyrizo7 <randyrizopdx@gmail.com>
2024-05-30 18:04:40 -0700

    added crud operation

---
+++ src/Enums/ProductTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Defines an enum for the different types
+    /// </summary>
+    public enum ProductTypeEnum
+    {
+        // Adding categories for ProductType list displayed on update page for admin to choose from
+        [Display(Name = "Non-dairy")] nondairy = 0,
+        [Display(Name = "dairy")] dairy = 1,
+        [Display(Name = "vegan")] vegan = 2,
+        [Display(Name = "protein")] protein = 3,
+
+    }
+}
+
+
